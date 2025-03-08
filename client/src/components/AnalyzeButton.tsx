589278689e9ea17f66072081372a6492a805577a
kpl9 <52469626+klouie15@users.noreply.github.com>
2025-03-08 15:55:33 -0800

    Conditionally render results

--- client/src/components/AnalyzeButton.tsx
+++ client/src/components/AnalyzeButton.tsx
@@ -1,8 +1,8 @@
 import analyzeImgSrc from '../images/analyze.png';
 
-function AnalyzeButton() {
+function AnalyzeButton({ onClick }: { onClick: () => void }) {
     return <>
-        <button className="analyzeButton">
+        <button className="analyzeButton" onClick={onClick}>
             <img src={analyzeImgSrc} className="analyzeImg" alt="Analyze Button" />
             <p>Analyze Email</p>
         </button>
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,18 +1,32 @@
 import '../index.css'
 import AnalyzeButton from "../components/AnalyzeButton.tsx";
 import Results from "../components/Results.tsx";
+import { useState } from "react";
 
 function Landing() {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [anxietyScore, setAnxietyScore] = useState(0);
+
+    function handleSubmit(): void {
+        // TODO: Call API to retrieve score
+
+        setIsSubmitted(true);
+        setAnxietyScore(50);
+    }
+
     return <>
-        <body>
+        <div>
             <h1>Anxietize</h1>
             <h2>Get started by writing an email</h2>
             <textarea />
             <div className="analyzeContainer">
-                <AnalyzeButton />
+                <AnalyzeButton onClick={() => handleSubmit()} />
             </div>
-            <Results anxietyScore={75} />
-        </body>
+
+            { isSubmitted ? (
+                <Results anxietyScore={anxietyScore} />
+            ) : null }
+        </div>
     </>
 }
 
