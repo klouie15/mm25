bb61270a01b323b4c9afdfca2d148bd8039f98e9
kpl9 <52469626+klouie15@users.noreply.github.com>
2025-03-08 16:35:20 -0800

    Scroll into view after rendering results

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,11 +1,13 @@
 import '../index.css'
 import AnalyzeButton from "../components/AnalyzeButton.tsx";
 import Results from "../components/Results.tsx";
-import { useState } from "react";
+import {RefObject, useEffect, useRef, useState} from "react";
 
 function Landing() {
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [anxietyScore, setAnxietyScore] = useState(0);
+
+    const resultsRef: RefObject<HTMLDivElement | null> = useRef<HTMLDivElement | null>(null);
 
     function handleSubmit(): void {
         // TODO: Call API to retrieve score
@@ -14,17 +16,25 @@ function Landing() {
         setAnxietyScore(50);
     }
 
+    useEffect(() : void => {
+        if (isSubmitted && resultsRef.current) {
+            resultsRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [isSubmitted]);
+
     return <>
         <div>
             <h1>Anxietize</h1>
             <h2>Get started by writing an email</h2>
             <textarea />
-            <div className="analyzeContainer">
+            <div className="analyzeButtonContainer">
                 <AnalyzeButton onClick={() => handleSubmit()} />
             </div>
 
             { isSubmitted ? (
-                <Results anxietyScore={anxietyScore} />
+                <div ref={resultsRef}>
+                    <Results anxietyScore={anxietyScore} />
+                </div>
             ) : null }
         </div>
     </>
