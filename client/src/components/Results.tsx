356f6a5f5946327cb07fc27258f9c9ef1ba97e68
kpl9 <52469626+klouie15@users.noreply.github.com>
2025-03-08 20:10:55 -0800

    Add animated error alert and fading results

--- client/src/components/Results.tsx
+++ client/src/components/Results.tsx
@@ -2,7 +2,7 @@ import ScoreBar from "./ScoreBar.tsx";
 
 function Results({ anxietyScore } : { anxietyScore: number }) {
     return <>
-        <h2>Results</h2>
+        <h2 className="mt-20">Results</h2>
         <div className="anxietyScoreContainer">
             <h3 className="mt-2 mr-5">Anxiety Score</h3>
             <ScoreBar value={anxietyScore} />
